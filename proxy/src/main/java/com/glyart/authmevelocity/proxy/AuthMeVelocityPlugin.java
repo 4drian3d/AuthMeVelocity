ccff7c3a116a61361f3cb422e83941904630dfb5
Adrianed04 <adrianed2004@gmail.com>
2021-06-19 21:48:33 -0500

    Update to Paper 1.17, Java 16, and compilation changes

--- proxy/src/main/java/com/glyart/authmevelocity/proxy/AuthMeVelocityPlugin.java
+++ proxy/src/main/java/com/glyart/authmevelocity/proxy/AuthMeVelocityPlugin.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-@Plugin(id = "authmevelocity", name = "AuthMeVelocity", version = "1.0.0", url = "https://github.com/Glyart/AuthMeVelocity", description = "This plugin adds the support for AuthMeReloaded to Velocity.", authors = {"xQuickGlare"})
 @Getter
 public class AuthMeVelocityPlugin {
 
