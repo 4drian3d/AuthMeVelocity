5221b66140ac9295ac25fcc39eb3fdb8c4039e42
4drian3d <adrianed2004@gmail.com>
2021-11-06 10:49:32 -0500

    Remove LegacyChannelIdentifier
    - Change loggedPlayers collection from HashMap to HashSet

--- proxy/src/main/java/com/glyart/authmevelocity/proxy/AuthMeVelocityPlugin.java
+++ proxy/src/main/java/com/glyart/authmevelocity/proxy/AuthMeVelocityPlugin.java
@@ -7,15 +7,14 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
-import com.velocitypowered.api.proxy.messages.LegacyChannelIdentifier;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 import org.slf4j.Logger;
 
 import de.leonhard.storage.Yaml;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class AuthMeVelocityPlugin {
@@ -23,7 +22,7 @@ public class AuthMeVelocityPlugin {
     private final Logger logger;
     private static Yaml config = new Yaml("config", "plugins/AuthmeVelocity");
 
-    protected static final List<UUID> loggedPlayers = Collections.synchronizedList(new ArrayList<>());
+    protected static final Set<UUID> loggedPlayers = Collections.synchronizedSet(new HashSet<UUID>());
 
     @Inject
     public AuthMeVelocityPlugin(ProxyServer server, Logger logger) {
@@ -34,7 +33,6 @@ public class AuthMeVelocityPlugin {
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
         proxy.getChannelRegistrar().register(
-            new LegacyChannelIdentifier("authmevelocity:main"),
             MinecraftChannelIdentifier.create("authmevelocity", "main"));
         proxy.getEventManager().register(this, new ProxyListener(proxy, logger));
         if(proxy.getPluginManager().getPlugin("fastlogin").isPresent()){
