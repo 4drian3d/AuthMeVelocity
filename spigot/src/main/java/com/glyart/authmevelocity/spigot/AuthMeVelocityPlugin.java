cc3803307ef95eaec23496e1c60517427298f062
4drian3d <adriangonzalesval@gmail.com>
2022-07-12 16:10:57 +0000

    fix(paper): Unregister plugin channels on plugin disable

--- spigot/src/main/java/com/glyart/authmevelocity/spigot/AuthMeVelocityPlugin.java
+++ spigot/src/main/java/com/glyart/authmevelocity/spigot/AuthMeVelocityPlugin.java
@@ -12,17 +12,26 @@ import org.jetbrains.annotations.NotNull;
 
 public class AuthMeVelocityPlugin extends JavaPlugin {
     private static final String CHANNEL = "authmevelocity:main";
+
     @Override
     public void onEnable() {
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, CHANNEL);
         this.getServer().getMessenger().registerIncomingPluginChannel(this, CHANNEL, new MessageListener());
         this.getServer().getPluginManager().registerEvents(new AuthMeListener(this), this);
 
-        if(this.getServer().getPluginManager().isPluginEnabled("MiniPlaceholders")){
+        if (this.getServer().getPluginManager().isPluginEnabled("MiniPlaceholders")) {
             AuthmePlaceholders.getExpansion().register();
         }
 
         this.getSLF4JLogger().info("AuthMeVelocity enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, CHANNEL);
+        this.getServer().getMessenger().unregisterIncomingPluginChannel(this, CHANNEL);
+
+        this.getSLF4JLogger().info("AuthmeVelocity disabled");
     }
 
     public void sendMessageToProxy(final Player player, @NotNull MessageType type, @NotNull String playername) {
