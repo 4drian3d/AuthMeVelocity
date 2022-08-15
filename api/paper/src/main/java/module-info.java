7a940ab9cff0fe54cc65b17c2431cfc4da4376d5
4drian3d <adriangonzalesval@gmail.com>
2022-08-15 14:28:13 +0000

    misc: Removed static module dependency

--- api/paper/src/main/java/module-info.java
+++ api/paper/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 /**AuthMeVelocity Paper API Module */
 @SuppressWarnings({"requires-automatic", "requires-transitive-automatic"})
 module me.adrianed.authmevelocity.api.paper {
-    requires static transitive org.bukkit;
+    requires transitive org.bukkit;
     requires static org.jetbrains.annotations;
     exports me.adrianed.authmevelocity.api.paper.event;
 }
--- api/velocity/src/main/java/module-info.java
+++ api/velocity/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 /**AuthMeVelocity Velocity API Module */
 @SuppressWarnings({"requires-automatic", "requires-transitive-automatic"})
 module me.adrianed.authmevelocity.api.velocity {
-    requires static transitive com.velocitypowered.api;
+    requires transitive com.velocitypowered.api;
     requires static org.jetbrains.annotations;
     exports me.adrianed.authmevelocity.api.velocity;
     exports me.adrianed.authmevelocity.api.velocity.event;
