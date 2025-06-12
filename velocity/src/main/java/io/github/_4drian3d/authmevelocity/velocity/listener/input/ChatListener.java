df17627624fb484be9ff84e170d1ab0e3c75d9ae
W1CK3D <w1ck3dworks@gmail.com>
2025-06-12 21:05:10 +0200

    Add chat option servers that dont require auth

--- velocity/src/main/java/io/github/_4drian3d/authmevelocity/velocity/listener/input/ChatListener.java
+++ velocity/src/main/java/io/github/_4drian3d/authmevelocity/velocity/listener/input/ChatListener.java
@@ -51,6 +51,14 @@ public final class ChatListener implements Listener<PlayerChatEvent> {
 
             plugin.logDebug(() -> "PlayerChatEvent | Player " + event.getPlayer().getUsername() + " is not logged");
 
+            final List<String> allowedServers = plugin.config().get().chat().serversThatDontRequireAuthForChat();
+            if (!allowedServers.isEmpty() && player.getCurrentServer().map(server -> 
+                    allowedServers.contains(server.getServerInfo().getName())).orElse(false)) {
+                plugin.logDebug(() -> "PlayerChatEvent | Player " + event.getPlayer().getUsername() + " is on allowed server for chat");
+                continuation.resume();
+                return;
+            }
+
             if (plugin.config().get().chat().enableAllowedChatPrefixes()
                     && !plugin.config().get().chat().allowedChatPrefixes().isEmpty()
                     && isMessageAllowed(event.getMessage(), plugin.config().get().chat().allowedChatPrefixes())) {
