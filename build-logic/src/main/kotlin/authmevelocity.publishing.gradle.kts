c2f5210d26498c3fb20485370d2f83494fd5d7f7
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-08-21 14:35:04 -0500

    release: Version 4.2.0

--- build-logic/src/main/kotlin/authmevelocity.publishing.gradle.kts
+++ build-logic/src/main/kotlin/authmevelocity.publishing.gradle.kts
@@ -1,76 +1,45 @@
+import java.time.LocalDate
+
 plugins {
     `java-library`
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish")
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, project.name, project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/4drian3d/AuthMeVelocity")
+        licenses {
+            license {
+                name.set("GNU General Public License version 3 or later")
+                url.set("https://opensource.org/licenses/GPL-3.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/4drian3d/AuthMeVelocity.git")
+            developerConnection.set("scm:git:ssh://git@github.com/4drian3d/AuthMeVelocity.git")
+            url.set("https://github.com/4drian3d/AuthMeVelocity")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/4drian3d/AuthMeVelocity/actions")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/4drian3d/AuthMeVelocity/issues")
+        }
+    }
 }
-
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-//            repositories {
-//                maven {
-//                    credentials {
-//                        username = property("sonatypeTokenUsername")?.toString() ?: ""
-//                        password = property("sonatypeTokenPassword")?.toString() ?: ""
-//                    }
-//
-//                    val central = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-//                    val snapshots = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-//
-//                    if (project.version.toString().endsWith("SNAPSHOT")) {
-//                        name = "SonatypeSnapshots"
-//                        setUrl(snapshots)
-//                    } else {
-//                        name = "OSSRH"
-//                        setUrl(central)
-//                    }
-//                }
-//            }
-//            from(components["java"])
-//            pom {
-//                url.set("https://github.com/4drian3d/AuthMeVelocity")
-//                licenses {
-//                    license {
-//                        name.set("GNU General Public License version 3 or later")
-//                        url.set("https://opensource.org/licenses/GPL-3.0")
-//                    }
-//                }
-//                scm {
-//                    connection.set("scm:git:https://github.com/4drian3d/AuthMeVelocity.git")
-//                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/AuthMeVelocity.git")
-//                    url.set("https://github.com/4drian3d/AuthMeVelocity")
-//                }
-//                developers {
-//                    developer {
-//                        id.set("4drian3d")
-//                        name.set("Adrian Gonzales")
-//                        email.set("adriangonzalesval@gmail.com")
-//                    }
-//                }
-//                issueManagement {
-//                    name.set("GitHub")
-//                    url.set("https://github.com/4drian3d/AuthMeVelocity/issues")
-//                }
-//                ciManagement {
-//                    name.set("GitHub Actions")
-//                    url.set("https://github.com/4drian3d/AuthMeVelocity/actions")
-//                }
-//                name.set(project.name)
-//                description.set(project.description)
-//                url.set("https://github.com/4drian3d/AuthMeVelocity")
-//            }
-//        }
-//    }
-//}
-//
-//signing {
-//    useGpgCmd()
-//    sign(configurations.archives.get())
-//    sign(publishing.publications["mavenJava"])
-//}
-
